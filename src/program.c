8b8b19553c6ea550c858209f6f225303b4b18b98
Tom <thomasfardell@outlook.com>
2025-07-22 19:38:21 +0100

    Ensure user input is safe

--- src/program.c
+++ src/program.c
@@ -5,48 +5,103 @@
 
 #define BUFFER_SIZE 20
 #define MAX_ID 1023
+#define MAX_AMOUNT 10000
 
 void clear_screen() { printf("\e[1;1H\e[2J"); }
 
-void add_transaction(chain *chn) {
+void clear_stdin() {
+  int c;
+  while ((c = fgetc(stdin)) != '\n' && c != EOF);
+}
+
+int get_payee_id() {
   char buffer[BUFFER_SIZE];
 
-  int payee_id;
   while (1) {
     printf("Enter payee ID > ");
     fgets(buffer, BUFFER_SIZE, stdin);
 
-    int found = sscanf(buffer, "%d", &payee_id);
-    if (found && 0 <= payee_id && payee_id <= MAX_ID) break;
+    // Iterate through the buffer to ensure it wasn't overfilled
+    int newline_found = 0;
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+      if (buffer[i] == '\n') {
+        int payee_id;
+        int found = sscanf(buffer, "%d", &payee_id);
+
+        if (found && 0 <= payee_id && payee_id <= MAX_ID) return payee_id;
+
+        newline_found = 1;
+        break;
+      }
+    }
+
+    // Only clear stdin if the input was too large, i.e. if no '\n' was found
+    if (!newline_found) clear_stdin();
 
     printf("ID must be a number between 0 and %d.\n", MAX_ID);
   }
+}
 
-  int payer_id;
+int get_payer_id() {
+  char buffer[BUFFER_SIZE];
+
   while (1) {
     printf("Enter payer ID > ");
     fgets(buffer, BUFFER_SIZE, stdin);
 
-    int found = sscanf(buffer, "%d", &payer_id);
-    if (found && 0 <= payer_id && payee_id <= MAX_ID) break;
+    int newline_found = 0;
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+      if (buffer[i] == '\n') {
+        int payer_id;
+        int found = sscanf(buffer, "%d", &payer_id);
+
+        if (found && 0 <= payer_id && payer_id <= MAX_ID) return payer_id;
+
+        newline_found = 1;
+        break;
+      }
+    }
+
+    if (!newline_found) clear_stdin();
 
     printf("ID must be a number between 0 and %d.\n", MAX_ID);
   }
+}
 
-  double amount;
+double get_amount() {
+  char buffer[BUFFER_SIZE];
+
   while (1) {
     printf("Enter amount > ");
     fgets(buffer, BUFFER_SIZE, stdin);
 
-    int found = sscanf(buffer, "%lf", &amount);
-    if (found && amount >= 0) break;
+    int newline_found = 0;
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+      if (buffer[i] == '\n') {
+        double amount;
+        int found = sscanf(buffer, "%lf", &amount);
 
-    printf("Amount must be a positive number.\n");
+        if (found && 0 <= amount && amount <= MAX_AMOUNT) return amount;
+
+        newline_found = 1;
+        break;
+      }
+    }
+
+    if (!newline_found) clear_stdin();
+
+    printf("Amount must be a number between 0 and %d.\n", MAX_AMOUNT);
   }
+}
 
-  printf("Transaction of " AMOUNT_FORMAT " from %d to %d added. Press ENTER to continue > ", amount, payer_id,
+void add_transaction(chain *chn) {
+  int payee_id = get_payee_id();
+  int payer_id = get_payer_id();
+  double amount = get_amount();
+
+  printf("\nTransaction of " AMOUNT_FORMAT " from %d to %d added.\nPress ENTER to continue > ", amount, payer_id,
          payee_id);
-  getchar();
+  fgetc(stdin);
 
   transaction trans = transaction_init(amount, payer_id, payee_id);
   chain_add_node(chn, trans);
